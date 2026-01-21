614c175e7e80332352b4ef0db17e058c658a0936
trefonskilucas-art <trefonskilucas@gmail.com>
2026-01-21 13:12:11 +0000

    ok

--- materiel.cpp
+++ materiel.cpp
@@ -4,7 +4,7 @@
 materiel::materiel (std::string nom, std::string type) : _nom(nom), _type(type){
     if (!isvalid()){
         std::cout << "erreur de type, mise a defaut en tant que materiel a type inconnu" << std::endl;
-        _
+        _type = "inconnu"
     }
 
 }
